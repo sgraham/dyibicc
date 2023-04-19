52f0b0e55a4fb759bf1f21c3c04b7c0fa0b581c3
Scott Graham <scott.github@h4ck3r.net>
2023-04-19 09:27:40 -0700

    Tidy up 'm' and define one of _DEBUG or NDEBUG

--- m
+++ m
@@ -1,3 +1,8 @@
 #!/bin/sh
+if [ $# -eq 0 ]
+then
+    echo "usage: m d|r|a [target]"
+    exit 1
+fi
 python3 src/gen.py
 third_party/ninja/ninja-linux -C out/l$1 $2 $3 $4 $5
--- m.bat
+++ m.bat
@@ -1,3 +1,7 @@
 @echo off
+if '%1==' goto :err
 call python src/gen.py
 call third_party\ninja\ninja-win.exe -C out\w%1 %2 %3 %4 %5
+goto :EOF
+:err
+echo usage: m d^|r^|a [target]
