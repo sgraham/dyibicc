992e9b78690fb1eaf7601941e158de2d7ee0498e
Scott Graham <scott.github@h4ck3r.net>
2023-10-18 12:26:13 -0700

    attempt to fix exit codes

--- m.bat
+++ m.bat
@@ -1,7 +1,7 @@
 @echo off
 if '%1==' goto :err
-call python src/gen.py
-call third_party\ninja\ninja-win.exe -C out\w%1 %2 %3 %4 %5
+call python src/gen.py || exit /b
+call third_party\ninja\ninja-win.exe -C out\w%1 %2 %3 %4 %5 || exit /b
 goto :EOF
 :err
 echo usage: m d^|r^|a [target]
